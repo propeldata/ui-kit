6cdd06eeb674298f616c6af4e3a9c59c65e7c85e
Mark Roberts <markandrusroberts@gmail.com>
2022-11-18 17:50:05 +0100

    chore: prepare to release

--- apps/nextjs/pages/index.tsx
+++ apps/nextjs/pages/index.tsx
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import '@propel-wc/time-series'
+import '@propeldata/wc-time-series'
 
 const IndexPage = () => <time-series accessToken={process.env.NEXT_PUBLIC_ACCESS_TOKEN}></time-series>
 
