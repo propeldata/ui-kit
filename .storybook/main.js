046c40e5c016713d2680e130971e8100842ec017
Jonatas Sales <jonatasvsales@gmail.com>
2022-11-19 17:50:23 -0300

    chore: added front-end tooling

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ['../packages/**/*.stories.mdx', '../packages/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  framework: '@storybook/react',
+  typescript: { reactDocgen: 'react-docgen' },
+  features: {
+    postcss: false
+  }
+}
