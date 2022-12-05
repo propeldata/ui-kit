ef09d4a0844937e2b5ee45190ff9234ac773ef54
Jonatas Sales <jonatasvsales@gmail.com>
2022-12-05 15:56:09 -0300

    refactor: addind old script to build aws

--- scripts/vercel-pre-install.sh
+++ scripts/vercel-pre-install.sh
@@ -5,6 +5,11 @@ unzip awscliv2.zip
 export AWS_ACCESS_KEY_ID=$BUILD_AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$BUILD_AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=$BUILD_AWS_DEFAULT_REGION
+aws codeartifact login --domain propel --repository propel --tool npm
+
+npm config set always-auth true
+
+# NOTE(mroberts): This is for Yarn v2 and above.
 export CODEARTIFACT_REPO=$(aws codeartifact get-repository-endpoint --domain propel --repository propel --format npm --output text)
 export CODEARTIFACT_AUTH_TOKEN=$(aws codeartifact get-authorization-token --domain propel --query authorizationToken --output text)
 yarn config set -H npmRegistryServer "$CODEARTIFACT_REPO"
