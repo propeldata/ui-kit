ba40e5be5c5ecd112a442e8a3ec8f3a99e17e06d
Jonatas Sales <jonatasvsales@gmail.com>
2023-01-06 14:19:37 -0300

    refactor: update gitignore

--- packages/core/graphql/src/index.ts
+++ packages/core/graphql/src/index.ts
@@ -1,3 +1,3 @@
-export type { RelativeTimeRange, TimeSeriesGranularity, TimeSeriesDocument } from './generated'
+export { RelativeTimeRange, TimeSeriesGranularity, TimeSeriesDocument } from './generated'
 
 export const PROPEL_GRAPHQL_API_ENDPOINT = 'https://api.us-east-2.dev.propeldata.com/graphql'
