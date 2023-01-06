db1a3cceece7793537b793225eeea6db32d22aed
Jonatas Sales <jonatasvsales@gmail.com>
2023-01-06 14:35:53 -0300

    refactor: added env files

--- packages/core/graphql/src/index.ts
+++ packages/core/graphql/src/index.ts
@@ -1,3 +1,3 @@
 export { RelativeTimeRange, TimeSeriesGranularity, TimeSeriesDocument } from './generated'
 
-export const PROPEL_GRAPHQL_API_ENDPOINT = 'https://api.us-east-2.dev.propeldata.com/graphql'
+export const PROPEL_GRAPHQL_API_ENDPOINT = process.env.PROPEL_GRAPHQL_API_ENDPOINT as string
