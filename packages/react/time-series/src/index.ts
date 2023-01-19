f90d4f6ed75832ce65d2261b9e3289f71d0569b2
Jonatas Sales <jonatasvsales@gmail.com>
2023-01-19 16:51:56 -0300

    fix: typo

--- packages/react/time-series/src/index.ts
+++ packages/react/time-series/src/index.ts
@@ -1,2 +1,2 @@
 export { TimeSeries } from './TimeSeries'
-export type { Styles, ChartVariant, TimeSeriesData } from './__types__'
+export type { Styles, ChartVariant, TimeSeriesData } from './types'
