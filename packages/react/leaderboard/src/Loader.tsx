918390e9f4dda3e9b4092e2eb1e3e61b4fc62bed
Jonatas Sales <jonatasvsales@gmail.com>
2023-03-20 10:16:53 -0300

    feat: added some aria attributes

--- packages/react/leaderboard/src/Loader.tsx
+++ packages/react/leaderboard/src/Loader.tsx
@@ -15,42 +15,45 @@ export function Loader(props: LoaderProps) {
   const height = styles?.canvas?.height || defaultChartHeight
 
   return (
-    <div
-      className={css`
-        width: ${width ? width + 'px' : '100%'};
-        height: ${height}px;
-        display: inline-block;
-        position: relative;
-        overflow: hidden;
-        background-color: #e9e9e9;
+    <div role="alert" aria-live="polite">
+      <p hidden>Loading...</p>
+      <div
+        className={css`
+          width: ${width ? width + 'px' : '100%'};
+          height: ${height}px;
+          display: inline-block;
+          position: relative;
+          overflow: hidden;
+          background-color: #e9e9e9;
 
-        &::after {
-          position: absolute;
-          top: 0;
-          right: 0;
-          bottom: 0;
-          left: 0;
-          transform: translateX(-100%);
-          background-image: linear-gradient(
-            90deg,
-            rgba(255, 255, 255, 0) 0,
-            rgba(255, 255, 255, 0.2) 20%,
-            rgba(255, 255, 255, 0.5) 60%,
-            rgba(255, 255, 255, 0)
-          );
-          animation: shimmer 1.5s infinite;
-          content: '';
-        }
-
-        @keyframes shimmer {
-          0% {
+          &::after {
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
             transform: translateX(-100%);
+            background-image: linear-gradient(
+              90deg,
+              rgba(255, 255, 255, 0) 0,
+              rgba(255, 255, 255, 0.2) 20%,
+              rgba(255, 255, 255, 0.5) 60%,
+              rgba(255, 255, 255, 0)
+            );
+            animation: shimmer 1.5s infinite;
+            content: '';
           }
-          100% {
-            transform: translateX(100%);
+
+          @keyframes shimmer {
+            0% {
+              transform: translateX(-100%);
+            }
+            100% {
+              transform: translateX(100%);
+            }
           }
-        }
-      `}
-    />
+        `}
+      />
+    </div>
   )
 }
