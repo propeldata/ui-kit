458a8287c4291a5099edc3edc403a2122d2f5bd3
Mark Andrus Roberts <markandrusroberts@gmail.com>
2023-06-26 17:30:45 +0200

    chore: use JavaScript instead of `ed` (Vercel doesn't have `ed`)

---
+++ packages/core/graphql/script.cjs
@@ -0,0 +1,8 @@
+const fs = require('node:fs')
+
+const generatedFilePath = process.argv[2]
+const generatedFileContent = fs.readFileSync(generatedFilePath).toString()
+fs.writeFileSync(
+  generatedFilePath,
+  generatedFileContent.replaceAll(/import { requestinit } from.+\r?\n/gi, '')
+)
