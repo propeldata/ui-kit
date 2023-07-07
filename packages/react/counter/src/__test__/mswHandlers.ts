e0632caf5977a2057d3f166f0009ec19a6405162
Felipe Cadavid <felipe.cadavid3@gmail.com>
2023-07-07 10:25:24 -0500

    fix: remove unnecessary console.log

--- packages/react/counter/src/__test__/mswHandlers.ts
+++ packages/react/counter/src/__test__/mswHandlers.ts
@@ -8,8 +8,6 @@ const handlers = [
   graphql.query('Counter', (req, res, ctx) => {
     const { metricName } = req.variables.counterInput
 
-    console.log(metricName)
-
     if (metricName === 'lack-of-data') {
       return res(
         ctx.data({
