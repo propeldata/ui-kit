73c4c8053008543c20b027fbc9f1496c7fcb1edb
Felipe Cadavid <felipe.cadavid3@gmail.com>
2023-11-24 11:50:16 -0500

    chore: add comment with code-gen issue

--- packages/ui-kit/src/helpers/graphql/script.cjs
+++ packages/ui-kit/src/helpers/graphql/script.cjs
@@ -7,5 +7,6 @@ fs.writeFileSync(
   generatedFilePath,
   generatedFileContent
     .replaceAll(/import { requestinit } from.+\r?\n/gi, '')
+    // https://github.com/dotansimha/graphql-code-generator-community/issues/509
     .replaceAll(/ variables]/gi, ' dataSource, variables]')
 )
