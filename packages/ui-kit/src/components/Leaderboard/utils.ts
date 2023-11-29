86eceda8603320393051b03cdb0589106b9c832f
Felipe Cadavid <felipe.cadavid3@gmail.com>
2023-11-29 17:00:03 -0500

    FIX: Table leaderboard breaking on query change (#137)
    * fix: safely set valuesbyrow
    
    * refactor: regenerate lock files

--- packages/ui-kit/src/components/Leaderboard/utils.ts
+++ packages/ui-kit/src/components/Leaderboard/utils.ts
@@ -15,9 +15,9 @@ export function getTableSettings(options: GetTableSettingsOptions) {
   const headersWithoutValue = headers?.slice(0, headers.length - 1)
   const valueHeader = headers?.[headers.length - 1]
 
-  const rowsWithoutValue = rows?.map((row) => row.slice(0, row.length - 1))
+  const rowsWithoutValue = rows?.map((row) => row.slice(0, row.length - 1)) ?? []
 
-  const valuesByRow = rows?.map((row) => (row[row.length - 1] === null ? null : row[row.length - 1]))
+  const valuesByRow = rows?.map((row) => (row[row.length - 1] === null ? null : row[row.length - 1])) ?? []
 
   const isValidValueBar = valuesByRow.every((value) => !isNaN(parseFloat(value)))
 
