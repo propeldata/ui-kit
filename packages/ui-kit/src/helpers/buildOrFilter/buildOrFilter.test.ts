7c1a9a7691d61405e7cabe557303a3e687e85ab0
Felipe Cadavid <felipe.cadavid3@gmail.com>
2024-01-23 08:51:15 -0500

    test: add test for buildOrFilter

--- packages/ui-kit/src/helpers/buildOrFilter/buildOrFilter.test.ts
+++ packages/ui-kit/src/helpers/buildOrFilter/buildOrFilter.test.ts
@@ -1,1 +1,29 @@
-// TODO: WRITE TESTS
+import { FilterOperator } from '../graphql'
+import { buildOrFilter } from './buildOrFilter'
+
+const columnName = 'some column'
+const valuesToFilter = ['value_1', 'value_2', 'value_3']
+
+describe('buildOrFilter', () => {
+  it('Should build filter correctly', () => {
+    const result = buildOrFilter(columnName, valuesToFilter)
+
+    expect(result).toEqual({
+      column: columnName,
+      operator: FilterOperator.Equals,
+      value: valuesToFilter[0],
+      or: [
+        {
+          column: columnName,
+          operator: FilterOperator.Equals,
+          value: valuesToFilter[1]
+        },
+        {
+          column: columnName,
+          operator: FilterOperator.Equals,
+          value: valuesToFilter[2]
+        }
+      ]
+    })
+  })
+})
