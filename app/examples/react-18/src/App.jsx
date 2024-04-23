aee73915cffac2bf4b3f22335c20b063181ff05a
Alexander Schwarzmann <hello@sashathor.com>
2024-04-23 13:42:49 +0200

    fix(ui-kit): missin Inter font

--- app/examples/react-18/src/App.jsx
+++ app/examples/react-18/src/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import { Dashboard } from 'dashboard-example'
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/700.css'
 
 const { REACT_APP_CLIENT_ID, REACT_APP_CLIENT_SECRET } = process.env
 
