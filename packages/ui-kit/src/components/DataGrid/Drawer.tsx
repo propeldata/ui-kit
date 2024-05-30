56ea8ae748330e3ed5e6144f112d9e24c0e0a1ef
Felipe Cadavid <felipe.cadavid3@gmail.com>
2024-05-30 09:49:00 -0500

    refactor: remove comments

--- packages/ui-kit/src/components/DataGrid/Drawer.tsx
+++ packages/ui-kit/src/components/DataGrid/Drawer.tsx
@@ -37,7 +37,6 @@ export function Drawer(props: DrawerProps) {
                   <CopyIcon />
                 </button>
               </div>
-              {/* <SizedTextArea className={componentStyles.value} readOnly>{getDisplayValue(cell.value)}</SizedTextArea> */}
               <code className={componentStyles.value}>{getDisplayValue(cell.value)}</code>
             </div>
             {row && idx < row.cells.length - 1 && <div className={componentStyles.divider}></div>}
@@ -47,20 +46,3 @@ export function Drawer(props: DrawerProps) {
     </div>
   )
 }
-
-/**
- * Textarea that automatically adjusts its height to fit its content.
- */
-// const SizedTextArea = forwardRef<HTMLTextAreaElement, ComponentPropsWithoutRef<'textarea'>>(function TextArea({ children, ...rest }, ref) {
-//   const innerRef = useRef<HTMLTextAreaElement>(null)
-
-//   const componentRef = useCombinedRefs(ref, innerRef)
-
-//   useEffect(() => {
-//     if (componentRef.current) {
-//       componentRef.current.style.height = `${componentRef.current.scrollHeight}px`
-//     }
-//   }, [componentRef])
-
-//   return <textarea ref={componentRef} {...rest}>{children}</textarea>
-// })
