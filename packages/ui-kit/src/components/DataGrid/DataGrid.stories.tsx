d0f62ea9d410ecea9634fdd8ff07926768c721c5
Felipe Cadavid <felipe.cadavid3@gmail.com>
2024-06-07 17:59:41 -0500

    feat: add static story

--- packages/ui-kit/src/components/DataGrid/DataGrid.stories.tsx
+++ packages/ui-kit/src/components/DataGrid/DataGrid.stories.tsx
@@ -142,3 +142,80 @@ export const CustomPaginationOptions: Story = {
   },
   render: (args) => <DataGrid {...args} />
 }
+
+export const Static: Story = {
+  args: {
+    headers: ['taco_name', 'restaurant_name', 'tortilla_name', 'restaurant_id', 'sauce_id'],
+    rows: [
+      [
+        'Breakfast',
+        'Los Compadres',
+        'Spinach',
+        '605cf627-794e-47f6-9f23-19c8dda7c24a',
+        '25cdef0f-c358-4788-9645-f9625b9a219f'
+      ],
+      [
+        'Veggie',
+        'Los Compadres',
+        'Whole Wheat',
+        '605cf627-794e-47f6-9f23-19c8dda7c24a',
+        'a37d3001-f953-47e3-b1ed-149f8897d094'
+      ],
+      [
+        'Al Pastor',
+        'Taqueria Cancun',
+        'Corn',
+        '9bdde1f8-cbe2-4dba-bca1-14352a152173',
+        '25cdef0f-c358-4788-9645-f9625b9a219f'
+      ],
+      [
+        'Breakfast',
+        'Los Compadres',
+        'Flour',
+        '605cf627-794e-47f6-9f23-19c8dda7c24a',
+        'eb08c249-6855-4074-a476-75ecce863a7c'
+      ],
+      [
+        'Shrimp',
+        'Los Compadres',
+        'Corn',
+        '605cf627-794e-47f6-9f23-19c8dda7c24a',
+        'a37d3001-f953-47e3-b1ed-149f8897d094'
+      ],
+      [
+        'Veggie',
+        'Los Compadres',
+        'Corn',
+        '605cf627-794e-47f6-9f23-19c8dda7c24a',
+        '2374b5f8-b939-49f1-b7d9-3001c37096b8'
+      ],
+      [
+        'Shrimp',
+        'Los Compadres',
+        'Corn',
+        '605cf627-794e-47f6-9f23-19c8dda7c24a',
+        '6c8f7f6a-25d9-4bdc-ae3b-6b92b808c1d3'
+      ],
+      [
+        'Chorizo',
+        'La Taqueria',
+        'Nopal',
+        'f38ed454-a907-43b4-b362-cd4dd197dd03',
+        'a37d3001-f953-47e3-b1ed-149f8897d094'
+      ],
+      [
+        'Shrimp',
+        'La Taqueria',
+        'Whole Wheat',
+        'f38ed454-a907-43b4-b362-cd4dd197dd03',
+        '2374b5f8-b939-49f1-b7d9-3001c37096b8'
+      ],
+      ['Veggie', 'La Taqueria', 'Corn', 'f38ed454-a907-43b4-b362-cd4dd197dd03', 'a37d3001-f953-47e3-b1ed-149f8897d094']
+    ],
+    paginationProps: {
+      defaultPageSize: 5,
+      pageSizeOptions: [5]
+    }
+  },
+  render: (args) => <DataGrid {...args} />
+}
