626cda3c8ed5b24b963b665de5cc361c87156865
Alexander Schwarzmann <hello@sashathor.com>
2024-07-31 16:20:37 +0200

    chore(storybook): docs about UI kit ver 0.6.0

--- packages/ui-kit/src/components/Typography/Typography.stories.tsx
+++ packages/ui-kit/src/components/Typography/Typography.stories.tsx
@@ -41,3 +41,19 @@ export const BigStory: Story = {
     </div>
   )
 }
+
+export const MixedWithdStory: Story = {
+  name: 'Mixed with weight',
+  tags: ['hidden'],
+  render: (args) => (
+    <div>
+      <Typography as="h2" size={6} weight="bold">
+        Typographic principles
+      </Typography>
+      <Typography as="p" size={2}>
+        The goal of typography is to relate font size, line height, and line width in a proportional way that maximizes
+        beauty and makes reading easier and more pleasant.
+      </Typography>
+    </div>
+  )
+}
