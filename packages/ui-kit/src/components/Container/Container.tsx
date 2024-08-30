906e7fc98e9626b70386a4b5e8abc7c4d26513e5
Felipe Cadavid <felipe.cadavid3@gmail.com>
2024-08-30 17:25:51 -0500

    feat: expose radix components

---
+++ packages/ui-kit/src/components/Container/Container.tsx
@@ -0,0 +1,5 @@
+import { Container as ContainerBase, ContainerProps } from '@radix-ui/themes'
+import '@radix-ui/themes/styles.css'
+
+export const Container = ContainerBase
+export type { ContainerProps }
