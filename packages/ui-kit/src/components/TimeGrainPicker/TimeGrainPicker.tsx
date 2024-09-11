4e7bc27cf0d61aca709430ed1e498288ef33521e
Felipe Cadavid <felipe.cadavid3@gmail.com>
2024-09-11 13:00:13 -0500

    fix: timegrainpicker options

--- packages/ui-kit/src/components/TimeGrainPicker/TimeGrainPicker.tsx
+++ packages/ui-kit/src/components/TimeGrainPicker/TimeGrainPicker.tsx
@@ -1,41 +1,59 @@
 'use client'
 
 import React, { useState } from 'react'
+import { useForwardedRefCallback } from '../../helpers'
 import { TimeSeriesGranularity } from '../../graphql'
 import { useFilters } from '../FilterProvider/useFilters'
-import { Select } from '../Select'
+import { Option, Select } from '../Select'
 import { OrderedTimeSeriesGranularity, TimeGrainPickerProps } from './TimeGrainPicker.types'
 import { granularityLabel } from './utils'
+import { useParsedComponentProps } from '../../themes'
 
 interface TimeGrainPickerOption {
   label: string
   value: TimeSeriesGranularity
 }
 
-export const TimeGrainPicker = React.forwardRef<HTMLButtonElement, TimeGrainPickerProps>(
-  ({ selectProps, options = OrderedTimeSeriesGranularity }, ref) => {
+export const TimeGrainPicker = React.forwardRef<HTMLDivElement, TimeGrainPickerProps>(
+  ({ selectProps, options = OrderedTimeSeriesGranularity, ...rest }, forwardedRef) => {
+    const selectOptions = React.useMemo(
+      () => options.map((option) => ({ label: granularityLabel[option], value: option })),
+      [options]
+    )
+
+    const { parsedPropsWithoutRest } = useParsedComponentProps(rest)
+    const { setRef } = useForwardedRefCallback(forwardedRef)
+
+    const selectRef = React.useRef<HTMLButtonElement | null>(null)
+
     const { granularity, setGranularity } = useFilters()
 
     const [selectedOption, setSelectedOption] = useState<TimeGrainPickerOption | null>(
-      granularity != null ? { label: granularityLabel[granularity], value: granularity } : null
+      selectOptions?.find((option) => option.value === granularity) ?? null
     )
 
     return (
-      <Select<TimeGrainPickerOption>
-        {...selectProps}
-        ref={ref}
-        onChange={(_, optionValue) => {
-          if (optionValue != null) {
+      <div ref={setRef} {...parsedPropsWithoutRest}>
+        <Select
+          {...selectProps}
+          ref={selectRef}
+          onChange={(_, optionValue) => {
+            if (optionValue == null) {
+              return
+            }
+
             setGranularity(optionValue.value)
-            setSelectedOption(optionValue)
-          }
-        }}
-        value={selectedOption}
-        options={options.map((option) => ({
-          label: granularityLabel[option],
-          value: { label: granularityLabel[option], value: option }
-        }))}
-      />
+            setSelectedOption(selectOptions.find((option) => option.value === optionValue.value) ?? null)
+          }}
+          value={selectedOption}
+        >
+          {selectOptions.map((option) => (
+            <Option key={option.label} value={option}>
+              {option.label}
+            </Option>
+          ))}
+        </Select>
+      </div>
     )
   }
 )
