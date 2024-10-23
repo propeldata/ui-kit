644d72261fac887f99d640aff27d0cc482208e19
Felipe Cadavid <felipe.cadavid3@gmail.com>
2024-10-23 14:29:49 -0500

    refactor: remove unnecessary imports

--- packages/ui-kit/src/components/shared.utils.ts
+++ packages/ui-kit/src/components/shared.utils.ts
@@ -1,6 +1,3 @@
-import { AccentColors, accentColors as accentColorsDict, handleArbitraryColor, ThemeTokenProps } from '../themes'
-import { ThemeStateProps } from './ThemeProvider'
-
 /**
  * It receives a camelCase or snake_case name and returns a readable format
  * @example
